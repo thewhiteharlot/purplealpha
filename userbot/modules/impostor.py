357a3948f3c551932be7ff60a4b63f3f903eeabd
thewhiteharlot <scryptt@gmail.com>
2021-01-28 00:55:53 +0000

    black/lint autofix

--- userbot/modules/impostor.py
+++ userbot/modules/impostor.py
@@ -1,6 +1,5 @@
 from telethon.tl.functions.account import UpdateProfileRequest
-from telethon.tl.functions.photos import (DeletePhotosRequest,
-                                          UploadProfilePhotoRequest)
+from telethon.tl.functions.photos import DeletePhotosRequest, UploadProfilePhotoRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import InputPhoto
 
@@ -19,7 +18,8 @@ async def impostor(event):
         await event.edit("**Voltando à minha verdadeira identidade...**")
         if not STORAGE.userObj:
             return await event.edit(
-                "**Você precisa se passar por um perfil antes de reverter!**")
+                "**Você precisa se passar por um perfil antes de reverter!**"
+            )
         await updateProfile(STORAGE.userObj, restore=True)
         return await event.edit("**Revertido com sucesso!**")
     elif inputArgs:
@@ -32,16 +32,16 @@ async def impostor(event):
         replyMessage = await event.get_reply_message()
         if replyMessage.sender_id is None:
             return await event.edit(
-                "**Não é possível se passar por administradores anônimos, RIP.**")
-        userObj = await event.client(GetFullUserRequest(replyMessage.sender_id)
-                                     )
+                "**Não é possível se passar por administradores anônimos, RIP.**"
+            )
+        userObj = await event.client(GetFullUserRequest(replyMessage.sender_id))
     else:
         return await event.edit(
-            "**Digite** `.help clone` **para aprender como usá-lo.**")
+            "**Digite** `.help clone` **para aprender como usá-lo.**"
+        )
 
     if not STORAGE.userObj:
-        STORAGE.userObj = await event.client(
-            GetFullUserRequest(event.sender_id))
+        STORAGE.userObj = await event.client(GetFullUserRequest(event.sender_id))
 
     LOGS.info(STORAGE.userObj)
 
@@ -51,36 +51,43 @@ async def impostor(event):
 
 
 async def updateProfile(userObj, restore=False):
-    firstName = "Deleted Account" if userObj.user.first_name is None else userObj.user.first_name
+    firstName = (
+        "Deleted Account"
+        if userObj.user.first_name is None
+        else userObj.user.first_name
+    )
     lastName = "" if userObj.user.last_name is None else userObj.user.last_name
     userAbout = userObj.about if userObj.about is not None else ""
     userAbout = "" if len(userAbout) > 70 else userAbout
     if restore:
-        userPfps = await bot.get_profile_photos('me')
+        userPfps = await bot.get_profile_photos("me")
         userPfp = userPfps[0]
         await bot(
-            DeletePhotosRequest(id=[
-                InputPhoto(id=userPfp.id,
-                           access_hash=userPfp.access_hash,
-                           file_reference=userPfp.file_reference)
-            ]))
+            DeletePhotosRequest(
+                id=[
+                    InputPhoto(
+                        id=userPfp.id,
+                        access_hash=userPfp.access_hash,
+                        file_reference=userPfp.file_reference,
+                    )
+                ]
+            )
+        )
     else:
         try:
             userPfp = userObj.profile_photo
             pfpImage = await bot.download_media(userPfp)
-            await bot(
-                UploadProfilePhotoRequest(await bot.upload_file(pfpImage)))
+            await bot(UploadProfilePhotoRequest(await bot.upload_file(pfpImage)))
         except BaseException:
             pass
     await bot(
-        UpdateProfileRequest(about=userAbout,
-                             first_name=firstName,
-                             last_name=lastName))
+        UpdateProfileRequest(about=userAbout, first_name=firstName, last_name=lastName)
+    )
 
 
-CMD_HELP.update({
-    "clone":
-    ">`.clone` (como uma resposta a uma mensagem de um usuário)\
+CMD_HELP.update(
+    {
+        "clone": ">`.clone` (como uma resposta a uma mensagem de um usuário)\
     \nUso: Rouba a identidade do usuário.\
     \n\n>`.impostor <nome do usuário/ID>`\
     \nUso: Rouba o nome de usuário/id fornecida.\
@@ -88,4 +95,5 @@ CMD_HELP.update({
     \nUso: Reverta para sua verdadeira identidade.\
     \n\n**Sempre restaure antes de executá-lo novamente.**\
 "
-})
+    }
+)
